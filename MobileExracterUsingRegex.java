242d4b0bb2191b1a2de4dd7a88ef6eb3d9a4ca1b
nitishdothalsi <51431740+nitishdothalsi@users.noreply.github.com>
2019-06-29 14:59:35 +0530

    Update MobileExracterUsingRegex.java
    You have to create input and output text file and enter your details in input.txt file

--- MobileExracterUsingRegex.java
+++ MobileExracterUsingRegex.java
@@ -1,19 +1,26 @@
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MobileExracterUsingRegex {
-    public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
         PrintWriter pw=null;
-        try {
             pw = new PrintWriter("output.txt");
-            Pattern p=Pattern.compile("(0|91)");
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(MobileExracterUsingRegex.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            pw.close();
-        }
+            Pattern p=Pattern.compile("(0|91)?[6-9][0-9]{9}");
+            FileReader fr=new FileReader("input.txt");
+            BufferedReader br=new BufferedReader(fr);
+            String line=br.readLine();
+            while (line!=null) {
+                //System.out.println(line);
+            	Matcher m=p.matcher(line);
+                    //System.out.println(m.start()+">>"+m.end());
+            	while (m.find()) {                    
+					pw.println(m.group());
+				}
+				line=br.readLine();				
+			}
+            pw.flush();
     }
 }
